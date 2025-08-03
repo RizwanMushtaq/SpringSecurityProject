ced0b3cc421c7d7881a21c704ad24329d8df138d
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-03 15:13:19 +0200

    added database connector and jpa orm dependencies

--- src/main/java/com/rizwanmushtaq/SpringSecurityProject/SecurityConfig.java
+++ src/main/java/com/rizwanmushtaq/SpringSecurityProject/SecurityConfig.java
@@ -1,30 +1,27 @@
 package com.rizwanmushtaq.SpringSecurityProject;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity(debug = true)
 @Configuration
 public class SecurityConfig {
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user1 = User
-        .withDefaultPasswordEncoder()
-        .username("riz1")
-        .password("123")
-        .roles("ADMIN", "USER")
-        .build();
-    UserDetails user2 = User
-        .withDefaultPasswordEncoder()
-        .username("riz2")
-        .password("123")
-        .roles("USER")
-        .build();
-    InMemoryUserDetailsManager inMemoryUserDetailsManager =
-        new InMemoryUserDetailsManager(user1, user2);
-    return inMemoryUserDetailsManager;
-  }
+//  @Bean
+//  public UserDetailsService userDetailsService() {
+//    UserDetails user1 = User
+//        .withDefaultPasswordEncoder()
+//        .username("riz1")
+//        .password("123")
+//        .roles("ADMIN", "USER")
+//        .build();
+//    UserDetails user2 = User
+//        .withDefaultPasswordEncoder()
+//        .username("riz2")
+//        .password("123")
+//        .roles("USER")
+//        .build();
+//    InMemoryUserDetailsManager inMemoryUserDetailsManager =
+//        new InMemoryUserDetailsManager(user1, user2);
+//    return inMemoryUserDetailsManager;
+//  }
 }
